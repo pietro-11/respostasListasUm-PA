da4e74def40033ecd3e0aa780f33b5f76b8f3f11
pietro-11 <166645182+pietro-11@users.noreply.github.com>
2024-04-10 16:56:09 -0300

    Create Triangulo.java

---
+++ Triangulo.java
@@ -0,0 +1,39 @@
+public class Triangulo {
+	private double ladoA;
+	private double ladoB;
+	private double ladoC;
+	
+	public Triangulo(double a, double b, double c) {
+		this.ladoA = a;
+		this.ladoB = b;
+		this.ladoC = c;
+	}
+	
+	public double getLadoA() {
+		return ladoA;
+	}
+	public void setLadoA(double a) {
+		this.ladoA = a;
+	}
+	
+	public double getLadoB() {
+		return ladoB;
+	}
+	public void setLadoB(double b) {
+		this.ladoB = b;
+	}
+	
+	public double getLadoC() {
+		return ladoC;
+	}
+	public void setLadoC(double c) {
+		this.ladoC = c;
+	}
+	
+		
+		public double calcPerimetro() {
+			double peri = (ladoA + ladoB + ladoC);
+			return peri;
+		}
+}
+
