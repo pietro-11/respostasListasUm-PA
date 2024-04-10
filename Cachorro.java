1e46bb1b7ae4b40918fa27ff970029c1c1b4e952
pietro-11 <166645182+pietro-11@users.noreply.github.com>
2024-04-10 16:50:05 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,36 @@
+public class Cachorro {
+	private String nome;
+	private String raca;
+	private int idade;
+	
+		public Cachorro(String n, String r, int i) {
+			this.nome = n;
+			this.raca = r;
+			this.idade = i;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String n) {
+			this.nome = n;
+		}
+		
+		public String getRaca() {
+			return raca;
+		}
+		public void setRaca(String r) {
+			this.raca = r;
+		}
+		public int getIdade() {
+			return idade;
+		}
+		public void setIdade(int i) {
+			this.idade = i;
+		}
+		
+			public String latir() {
+				return "Au au!";
+			}
+}
+
